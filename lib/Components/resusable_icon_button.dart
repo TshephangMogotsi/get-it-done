9c25b556a60bfd4bd49df7b49889168af2e3b703
TshephangMogotsi <zinjie7@gmail.com>
2022-07-19 09:11:59 +0200

    added background svg cirlce elements and created a resusable icon button

--- lib/Components/resusable_icon_button.dart
+++ lib/Components/resusable_icon_button.dart
@@ -6,20 +6,23 @@ class ReusableElevatedButton extends StatelessWidget {
     required this.buttonColor,
     required this.label,
     required this.onPressed,
+    required this.icon,
   }) : super(key: key);
 
   final Color buttonColor;
   final String label;
   final VoidCallback onPressed;
+  final Widget icon;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
+    return ElevatedButton.icon(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
           primary: buttonColor, minimumSize: const Size(280, 50)),
-      child: Text(label,
+      label: Text(label,
           style: const TextStyle(color: Colors.white, fontSize: 16)),
+      icon: icon,
     );
   }
 }
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get_it_done/Components/reusable_button.dart';
 import 'package:get_it_done/screens/create_account_screen.dart';
 import 'package:get_it_done/screens/login.dart';
@@ -9,93 +10,120 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
           children: [
+            Positioned(
+              right: 200,
+              bottom: -210,
+              child: SvgPicture.asset(
+                'assets/Circle.svg',
+              ),
+            ),
+            Positioned(
+              left: 160,
+              top: -170,
+              child: SvgPicture.asset(
+                'assets/Circle.svg',
+              ),
+            ),
             Padding(
-              padding: const EdgeInsets.only(top: 20.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children:  [
-                  InkWell(
-                    onTap: (){
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => const Login()));
-                    },
-                    child: const Text(
-                      'Log in',
-                      style:  TextStyle(
-                          color: Color.fromARGB(255, 90, 90, 90), fontSize: 20),
+              padding:
+                  const EdgeInsets.only(left: 30, bottom: 30.0, right: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => const Login()));
+                          },
+                          child: const Text(
+                            'Log in',
+                            style: TextStyle(
+                                color: Color.fromARGB(255, 90, 90, 90),
+                                fontSize: 20),
+                          ),
+                        ),
+                      ],
                     ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      //We take the image from the assets
+                      Image.asset(
+                        'assets/bee.png',
+                        height: 150,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const Text(
+                        'Welcome to GET IT DONE',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'You never know when you might even land a job',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(color: Colors.black, fontSize: 14),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+
+                      ReusableElevatedButton(
+                          buttonColor: const Color(0xFFFFA600),
+                          label: 'Create account',
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (_) => CreateAccount()));
+                          }),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ReusableElevatedButton(
+                        buttonColor: const Color(0xFF2B5865),
+                        label: 'Continue with Google',
+                        onPressed: () {},
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => const MoreOptionsScreen()));
+                        },
+                        child: const Text(
+                          'More options',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(color: Colors.black, fontSize: 14),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    '''By tapping create account or more options, I agree to \nGet It Done Terms Of Service,Payment terms of service,Privacy Policy and Non-discrimination Policy''',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.black, fontSize: 12),
                   ),
                 ],
               ),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                //We take the image from the assets
-                Image.asset(
-                  'assets/bee.png',
-                  height: 150,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  'Welcome to GET IT DONE',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black, fontSize: 18),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'You never know when you might even land a job',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(color: Colors.black, fontSize: 14),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-
-                ReusableElevatedButton(
-                    buttonColor: const Color(0xFFFFA600),
-                    label: 'Create account',
-                    onPressed: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => CreateAccount()));
-                    }),
-                const SizedBox(
-                  height: 10,
-                ),
-                ReusableElevatedButton(
-                  buttonColor: const Color(0xFF2B5865),
-                  label: 'Continue with Google',
-                  onPressed: () {},
-                ),
-                 const SizedBox(
-                  height: 15,
-                ),
-                GestureDetector(
-                  onTap: (){
-                    Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => const MoreOptionsScreen()));
-                  },
-                  child: const Text(
-                    'More options',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(color: Colors.black, fontSize: 14),
-                  ),
-                ),
-              ],
-              
-            ),
-            const Text(
-              '''By tapping create account or more options, I agree to \nGet It Done Terms Of Service,Payment terms of service,Privacy Policy and Non-discrimination Policy''',
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black, fontSize: 12),
             ),
           ],
         ),
